437dd1bffb9dcaaaf1a50cbef226f896e49f4b82
Abhishek Adhikary <abhishek.112031@gmail.com>
2023-05-22 09:10:52 +0530

    post product details-Product Model

--- models/product.js
+++ models/product.js
@@ -1,27 +1,28 @@
-const Sequelize = require('sequelize');
+const getdb=require('../util/database').getdb; //allows us to connect with database;
 
-const sequelize = require('../util/database');
 
-const Product = sequelize.define('product', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  title: Sequelize.STRING,
-  price: {
-    type: Sequelize.DOUBLE,
-    allowNull: false
-  },
-  imageUrl: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  description: {
-    type: Sequelize.STRING,
-    allowNull: false
+class Product{
+  constructor(title,price,imageUrl,description){
+    this.title=title;
+    this.price=price;
+    this.imageUrl=imageUrl;
+    this.description=description;
   }
-});
+
+  save(){
+
+    const db=getdb();
+    return db.collection('products')
+    .insertOne(this)
+    .then(result=>{
+      console.log("---->",result);
+    })
+    .catch(err=>{
+      console.log(err);
+    })
+
+  }
+}
+
 
 module.exports = Product;
--- util/database.js
+++ util/database.js
@@ -1,8 +1,28 @@
-const Sequelize = require('sequelize');
+const mongodb=require('mongodb');
+const MongoClient=mongodb.MongoClient;
 
-const sequelize = new Sequelize('ecommerce-01', 'root', 'mysql@2022', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
 
-module.exports = sequelize;
+let _db;
+
+const mongoConnect=(callback)=>{
+
+  MongoClient.connect('mongodb+srv://abhishek112031:ecomdatabase@cluster0.ynvtbdb.mongodb.net/shop?retryWrites=true&w=majority').then(client=>{
+    console.log('Connected!!');
+    _db=client.db();
+    callback();
+  }).catch(err=>{
+    console.log(err);
+    throw err;
+  });
+}
+
+const getdb=()=>{
+  if(_db){
+    return _db;
+  }
+  throw 'no db found!'
+}
+
+
+exports.mongoConnect=mongoConnect;
+exports.getdb=getdb;

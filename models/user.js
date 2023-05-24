68a257681c3a2ffad8644cec6be58c55f1be5cfd
Abhishek Adhikary <abhishek.112031@gmail.com>
2023-05-24 16:07:30 +0530

    del items from the cart

--- models/user.js
+++ models/user.js
@@ -47,6 +47,14 @@ userSchema.methods.addToCart=function(product){
 
 }
 
+userSchema.methods.deleteFromCart=function(productId){
+    const updatedCartItems=this.cart.items.filter(item=>{
+              return item.productId.toString() !==productId.toString()
+            });
+    this.cart.items=updatedCartItems;
+    return this.save();        
+}
+
 module.exports=mongoose.model('User',userSchema);
 
 
